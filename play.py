b9963e27046bcca2ff71a3d266fbcb4eaf7a32d0
lakazatong <Bo_wood@outlook.fr>
2024-07-21 06:13:38 +0200

    fixed ctrl and shift, improved CLI, added data in gitignore, changed the recording stop condition of keyboard to releasing a 2s 'esc' hold to match the releasing of a 2s right click hold for consistency, added the possibility to have a custom delta time for replaying recordings, TODO: play.py still messy and a lot of code duplication, further testing

--- play.py
+++ play.py
@@ -1,66 +1,109 @@
 from pynput.mouse import Button, Controller as MouseController
 from pynput.keyboard import Key, Controller as KeyboardController
-import time
-import json
-import sys
+import time, json, sys
+
+# parse args
 
 n = len(sys.argv)
 
-if n < 3:
-    exit("Takes two arguments - name of recording to play and number of times to play it")
+if n != 4 and n != 5:
+	exit("Usage: python play.py {name of recording} {number of plays} {delay before playing (in seconds)} [delta time in between two action (in ms, will use recoding's delta times otherwise)]")
 
-if n > 3:
-    exit("Only takes two argument - name of recording to play and number of times to play it")
+name_of_recording = "data/" + str(sys.argv[1]) +'.txt'
+number_of_plays = int(sys.argv[2])
+delay = int(sys.argv[3])
+dt = int(sys.argv[4]) if n == 5 else None
 
-if n == 3:
-    name_of_recording = "data/" + str(sys.argv[1]) +'.txt'
-    number_of_plays = int(sys.argv[2])
+if number_of_plays <= 0:
+	exit("number of plays must be a positive number")
 
-with open(name_of_recording) as json_file:
-    data = json.load(json_file)
+if delay < 0:
+	exit("delay must be 0 or a positive number (in seconds)")
 
-special_keys = {"Key.shift": Key.shift, "Key.tab": Key.tab, "Key.caps_lock": Key.caps_lock, "Key.ctrl": Key.ctrl, "Key.alt": Key.alt, "Key.cmd": Key.cmd, "Key.cmd_r": Key.cmd_r, "Key.alt_r": Key.alt_r, "Key.ctrl_r": Key.ctrl_r, "Key.shift_r": Key.shift_r, "Key.enter": Key.enter, "Key.backspace": Key.backspace, "Key.f19": Key.f19, "Key.f18": Key.f18, "Key.f17": Key.f17, "Key.f16": Key.f16, "Key.f15": Key.f15, "Key.f14": Key.f14, "Key.f13": Key.f13, "Key.media_volume_up": Key.media_volume_up, "Key.media_volume_down": Key.media_volume_down, "Key.media_volume_mute": Key.media_volume_mute, "Key.media_play_pause": Key.media_play_pause, "Key.f6": Key.f6, "Key.f5": Key.f5, "Key.right": Key.right, "Key.down": Key.down, "Key.left": Key.left, "Key.up": Key.up, "Key.page_up": Key.page_up, "Key.page_down": Key.page_down, "Key.home": Key.home, "Key.end": Key.end, "Key.delete": Key.delete, "Key.space": Key.space}
+if dt < 0:
+	exit("delta time must be 0 or a positive number (in ms)")
+
+dt /= 1000
+
+try:
+	with open(name_of_recording) as json_file:
+		data = json.load(json_file) 
+except FileNotFoundError:
+	exit(f'recording "{name_of_recording}" not found')
+
+special_keys = {"Key.shift": Key.shift, "Key.tab": Key.tab, "Key.caps_lock": Key.caps_lock, "Key.ctrl": Key.ctrl, "Key.alt": Key.alt, "Key.cmd": Key.cmd, "Key.cmd_r": Key.cmd_r, "Key.alt_l": Key.alt_l, "Key.alt_r": Key.alt_r, "Key.ctrl_l": Key.ctrl_l, "Key.ctrl_r": Key.ctrl_r, "Key.shift_r": Key.shift_r, "Key.enter": Key.enter, "Key.backspace": Key.backspace, "Key.f19": Key.f19, "Key.f18": Key.f18, "Key.f17": Key.f17, "Key.f16": Key.f16, "Key.f15": Key.f15, "Key.f14": Key.f14, "Key.f13": Key.f13, "Key.media_volume_up": Key.media_volume_up, "Key.media_volume_down": Key.media_volume_down, "Key.media_volume_mute": Key.media_volume_mute, "Key.media_play_pause": Key.media_play_pause, "Key.f6": Key.f6, "Key.f5": Key.f5, "Key.right": Key.right, "Key.down": Key.down, "Key.left": Key.left, "Key.up": Key.up, "Key.page_up": Key.page_up, "Key.page_down": Key.page_down, "Key.home": Key.home, "Key.end": Key.end, "Key.delete": Key.delete, "Key.space": Key.space}
 
 mouse = MouseController()
 keyboard = KeyboardController()
 
-for loop in range(number_of_plays):
-    for index, obj in enumerate(data):
-        action, _time= obj['action'], obj['_time']
-        try:
-            next_movement = data[index+1]['_time']
-            pause_time = next_movement - _time
-        except IndexError as e:
-            pause_time = 1
-        
-        if action == "pressed_key" or action == "released_key":
-            key = obj['key'] if 'Key.' not in obj['key'] else special_keys[obj['key']]
-            print("action: {0}, time: {1}, key: {2}".format(action, _time, str(key)))
-            if action == "pressed_key":
-                keyboard.press(key)
-            else:
-                keyboard.release(key)
-            time.sleep(pause_time)
+time.sleep(delay)
 
+if dt == None:
+	for loop in range(number_of_plays):
+		for index, obj in enumerate(data):
+			action, _time = obj['action'], obj['time']
+			try:
+				next_movement = data[index+1]['time']
+				pause_time = next_movement - _time
+			except IndexError as e:
+				pause_time = 1
+			
+			if action == "pressed_key" or action == "released_key":
+				key = obj['key'] if 'Key.' not in obj['key'] else special_keys[obj['key']]
+				print("action: {}, time: {}, key: {}".format(action, _time, str(key)))
+				if action == "pressed_key":
+					keyboard.press(key)
+				else:
+					keyboard.release(key)
+				time.sleep(pause_time)
 
-        else:
-            move_for_scroll = True
-            x, y = obj['x'], obj['y']
-            if action == "scroll" and index > 0 and (data[index - 1]['action'] == "pressed" or data[index - 1]['action'] == "released"):
-                if x == data[index - 1]['x'] and y == data[index - 1]['y']:
-                    move_for_scroll = False
-            print("x: {0}, y: {1}, action: {2}, time: {3}".format(x, y, action, _time))
-            mouse.position = (x, y)
-            if action == "pressed" or action == "released" or action == "scroll" and move_for_scroll == True:
-                time.sleep(0.1)
-            if action == "pressed":
-                mouse.press(Button.left if obj['button'] == "Button.left" else Button.right)
-            elif action == "released":
-                mouse.release(Button.left if obj['button'] == "Button.left" else Button.right)
-            elif action == "scroll":
-                horizontal_direction, vertical_direction = obj['horizontal_direction'], obj['vertical_direction']
-                mouse.scroll(horizontal_direction, vertical_direction)
-            time.sleep(pause_time)
-    
+			else:
+				move_for_scroll = True
+				x, y = obj['x'], obj['y']
+				if action == "scroll" and index > 0 and (data[index - 1]['action'] == "pressed" or data[index - 1]['action'] == "released"):
+					if x == data[index - 1]['x'] and y == data[index - 1]['y']:
+						move_for_scroll = False
+				print("x: {}, y: {}, action: {}, time: {}".format(x, y, action, _time))
+				mouse.position = (x, y)
+				if action == "pressed" or action == "released" or action == "scroll" and move_for_scroll == True:
+					time.sleep(0.1)
+				if action == "pressed":
+					mouse.press(Button.left if obj['button'] == "Button.left" else Button.right)
+				elif action == "released":
+					mouse.release(Button.left if obj['button'] == "Button.left" else Button.right)
+				elif action == "scroll":
+					horizontal_direction, vertical_direction = obj['horizontal_direction'], obj['vertical_direction']
+					mouse.scroll(horizontal_direction, vertical_direction)
+				time.sleep(pause_time)
+else:
+	for loop in range(number_of_plays):
+		for index, obj in enumerate(data):
+			action = obj['action']
+			
+			if action == "pressed_key" or action == "released_key":
+				key = obj['key'] if 'Key.' not in obj['key'] else special_keys[obj['key']]
+				print("action: {}, key: {}".format(action, str(key)))
+				if action == "pressed_key":
+					keyboard.press(key)
+				else:
+					keyboard.release(key)
 
-
+			else:
+				move_for_scroll = True
+				x, y = obj['x'], obj['y']
+				if action == "scroll" and index > 0 and (data[index - 1]['action'] == "pressed" or data[index - 1]['action'] == "released"):
+					if x == data[index - 1]['x'] and y == data[index - 1]['y']:
+						move_for_scroll = False
+				print("x: {}, y: {}, action: {}".format(x, y, action))
+				mouse.position = (x, y)
+				if action == "pressed" or action == "released" or action == "scroll" and move_for_scroll == True:
+					time.sleep(0.1)
+				if action == "pressed":
+					mouse.press(Button.left if obj['button'] == "Button.left" else Button.right)
+				elif action == "released":
+					mouse.release(Button.left if obj['button'] == "Button.left" else Button.right)
+				elif action == "scroll":
+					horizontal_direction, vertical_direction = obj['horizontal_direction'], obj['vertical_direction']
+					mouse.scroll(horizontal_direction, vertical_direction)
+			
+			time.sleep(dt)
--- record.py
+++ record.py
@@ -1,93 +1,221 @@
-from pynput import mouse
-from pynput import keyboard
-import time
-import json
-import sys
+from pynput import mouse, keyboard
+from pynput.keyboard import Key
+import time, json, sys
+
+# parse args
 
 n = len(sys.argv)
 
-if n < 2:
-    exit("Takes a compulsory argument - name of recording, and optional argument - record-all")
+if n != 4 and n != 5:
+	exit('Usage: python record.py {"keyboard", "mouse" or "both"} {name of recording} {delay before recording (in seconds)} [record-mouse-movement]')
 
-if n > 3:
-    exit("Only takes two arguments - name of recording and (optional) record-all")
+what_to_record = str(sys.argv[1])
+if what_to_record not in ["keyboard", "mouse", "both"]:
+	exit('what to record must be either "keyboard", "mouse" or "both"')
+record_keyboard = what_to_record == "both" or what_to_record == "keyboard"
+record_mouse = what_to_record == "both" or what_to_record == "mouse"
+name_of_recording = str(sys.argv[2])
+delay = int(sys.argv[3])
+record_mouse_movement = str(sys.argv[4]) == "record-mouse-movement" if n == 5 else False
 
-if n == 2:
-    name_of_recording = str(sys.argv[1])
-    record_all = False
-if n == 3:
-    if str(sys.argv[2]) != "record-all":
-        exit("The second argument given must be 'record-all', otherwise only pass the name of recording as a parameter")
-    name_of_recording = str(sys.argv[1])
-    record_all = True
-
-print("Hold right click for more than 2 seconds (and then release) to end the recording for mouse and click 'esc' to end the recording for keyboard (both are needed to finish recording)")
+if delay < 0:
+	exit("delay must be 0 or a positive number (in seconds)")
 
 storage = []
-count = 0
+
+# keyboard related callbacks
+
+ctrl_keys = {
+	"'\\x01'": 'a',
+	"'\\x1a'": 'z',
+	"'\\x05'": 'e',
+	"'\\x12'": 'r',
+	"'\\x14'": 't',
+	"'\\x19'": 'y',
+	"'\\x15'": 'u',
+	"'\\t'": 'i',
+	"'\\x0f'": 'o',
+	"'\\x10'": 'p',
+	"'\\x11'": 'q',
+	"'\\x13'": 's',
+	"'\\x04'": 'd',
+	"'\\x06'": 'f',
+	"'\\x07'": 'g',
+	"'\\x08'": 'h',
+	"'\\n'": 'j',
+	"'\\x0b'": 'k',
+	"'\\x0c'": 'l',
+	"'\\r'": 'm',
+	"'\\x17'": 'w',
+	"'\\x18'": 'x',
+	"'\\x03'": 'c',
+	"'\\x16'": 'v',
+	"'\\x02'": 'b',
+	"'\\x0e'": 'n'
+}
+
+ctrl_pressed = False
+shift_pressed = False
 
 def on_press(key):
-    try:
-        json_object = {'action':'pressed_key', 'key':key.char, '_time': time.time()}
-    except AttributeError:
-        if key == keyboard.Key.esc:
-            return False
-        json_object = {'action':'pressed_key', 'key':str(key), '_time': time.time()}
-    storage.append(json_object)
+	global ctrl_pressed, shift_pressed
+	
+	key_str = str(key)
+	done = False
+	
+	if key_str == 'Key.ctrl_l' or key_str == 'Key.ctrl_r':
+		if not ctrl_pressed:
+			json_object = {'action': 'pressed_key', 'key': 'Key.ctrl_l', 'time': time.time()}
+			storage.append(json_object)
+		ctrl_pressed = True
+		print("> CTRL ON <")
+		done = True
+	
+	if key_str == 'Key.shift':
+		if not shift_pressed:
+			json_object = {'action': 'pressed_key', 'key': 'Key.shift', 'time': time.time()}
+			storage.append(json_object)
+		shift_pressed = True
+		print("> SHIFT ON <")
+		done = True
+
+	if done: return
+
+	if ctrl_pressed:
+		# if key_str not in ctrl_keys:
+		# 	print("unsupported ctrl key ({})".format(key_str))
+		# 	return
+
+		# risky, for debugging I would recommend uncommenting the code above and see
+		# this is good enough for now (we might end up hardcoding every key in ctrl_keys)
+		ctrl_key = ctrl_keys[key_str] if key_str in ctrl_keys else key_str
+		
+		json_object = {'action': 'pressed_key', 'key': ctrl_key, 'time': time.time()}
+	else:
+		try:
+			json_object = {'action': 'pressed_key', 'key': key.char, 'time': time.time()}
+		except AttributeError:
+			json_object = {'action': 'pressed_key', 'key': str(key), 'time': time.time()}
+	
+	to_print = json_object['key']
+	if shift_pressed: to_print = "shift+" + to_print
+	if ctrl_pressed: to_print = "ctrl+" + to_print
+	print(to_print)
+	
+	storage.append(json_object)
 
 def on_release(key):
-    try:
-        json_object = {'action':'released_key', 'key':key.char, '_time': time.time()}
-    except AttributeError:
-        json_object = {'action':'released_key', 'key':str(key), '_time': time.time()}
-    storage.append(json_object)
-        
+	global ctrl_pressed, shift_pressed
+	
+	key_str = str(key)
+	done = False
+
+	if key_str == 'Key.ctrl_l' or key_str == 'Key.ctrl_r':
+		if ctrl_pressed:
+			json_object = {'action': 'released_key', 'key': 'Key.ctrl_l', 'time': time.time()}
+			storage.append(json_object)
+		ctrl_pressed = False
+		print("> CTRL OFF <")
+		done = True
+	
+	if key_str == 'Key.shift':
+		if shift_pressed:
+			json_object = {'action': 'released_key', 'key': 'Key.shift', 'time': time.time()}
+			storage.append(json_object)
+		shift_pressed = False
+		print("> SHIFT OFF <")
+		done = True
+
+	if done: return
+
+	if ctrl_pressed:
+		if key_str not in ctrl_keys: return
+		json_object = {'action': 'released_key', 'key': ctrl_keys[key_str], 'time': time.time()}
+	else:
+		try:
+			json_object = {'action': 'released_key', 'key': key.char, 'time': time.time()}
+		except AttributeError:
+			json_object = {'action': 'released_key', 'key': str(key), 'time': time.time()}
+	
+	# detect 2s long 'esc' press
+	if len(storage) > 0 and json_object['key'] == 'Key.esc':
+		i = len(storage) - 1
+		while storage[i - 1]['action'] == 'pressed_key' and storage[i - 1]['key'] == 'Key.esc':
+			if i == 0: break
+			i -= 1
+		last_esc_pressed_time = storage[i]['time']
+		print(len(storage), i, json_object['time'], last_esc_pressed_time)
+		if json_object['time'] - last_esc_pressed_time > 2:
+			for _ in range(len(storage) - i):
+				storage.pop()
+			return False
+
+	storage.append(json_object)
+
+# mouse related callbacks
 
 def on_move(x, y):
-    if (record_all) == True:
-        if len(storage) >= 1:
-            if storage[-1]['action'] != "moved":
-                json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
-                storage.append(json_object)
-            elif storage[-1]['action'] == "moved" and time.time() - storage[-1]['_time'] > 0.02:
-                json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
-                storage.append(json_object)
-        else:
-            json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
-            storage.append(json_object)
-    else:
-        if len(storage) >= 1:
-            if (storage[-1]['action'] == "pressed" and storage[-1]['button'] == 'Button.left') or (storage[-1]['action'] == "moved" and time.time() - storage[-1]['_time'] > 0.02):
-                json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
-                storage.append(json_object)
+	if record_mouse_movement == True:
+		if len(storage) >= 1:
+			if storage[-1]['action'] != "moved":
+				json_object = {'action': 'moved', 'x': x, 'y': y, 'time': time.time()}
+				storage.append(json_object)
+			elif storage[-1]['action'] == "moved" and time.time() - storage[-1]['time'] > 0.02:
+				json_object = {'action': 'moved', 'x': x, 'y': y, 'time': time.time()}
+				storage.append(json_object)
+		else:
+			json_object = {'action': 'moved', 'x': x, 'y': y, 'time': time.time()}
+			storage.append(json_object)
+	else:
+		if len(storage) >= 1:
+			if (storage[-1]['action'] == "pressed" and storage[-1]['button'] == 'Button.left') or (storage[-1]['action'] == "moved" and time.time() - storage[-1]['time'] > 0.02):
+				json_object = {'action': 'moved', 'x': x, 'y': y, 'time': time.time()}
+				storage.append(json_object)
 
 def on_click(x, y, button, pressed):
-    json_object = {'action':'pressed' if pressed else 'released', 'button':str(button), 'x':x, 'y':y, '_time':time.time()}
-    storage.append(json_object)
-    if len(storage) > 1:
-        if storage[-1]['action'] == 'released' and storage[-1]['button'] == 'Button.right' and storage[-1]['_time'] - storage[-2]['_time'] > 2:
-            with open('data/{}.txt'.format(name_of_recording), 'w') as outfile:
-                json.dump(storage, outfile)
-            return False
-
+	json_object = {'action': 'pressed' if pressed else 'released', 'button': str(button), 'x': x, 'y': y, 'time': time.time()}
+	
+	# detect 2s right click hold
+	if len(storage) > 1:
+		if json_object['action'] == 'released' and json_object['button'] == 'Button.right' and json_object['time'] - storage[-1]['time'] > 2:
+			storage.pop()
+			return False
+	
+	json_object = {'action': 'pressed' if pressed else 'released', 'button': str(button), 'x': x, 'y': y, 'time': time.time()}
+	storage.append(json_object)
 
 def on_scroll(x, y, dx, dy):
-    json_object = {'action': 'scroll', 'vertical_direction': int(dy), 'horizontal_direction': int(dx), 'x':x, 'y':y, '_time': time.time()}
-    storage.append(json_object)
+	json_object = {'action': 'scroll', 'vertical_direction': int(dy), 'horizontal_direction': int(dx), 'x': x, 'y': y, 'time': time.time()}
+	storage.append(json_object)
 
+# start recordings
 
-# Collect events from keyboard until esc
-# Collect events from mouse until scroll
-keyboard_listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
+time.sleep(delay)
 
-mouse_listener = mouse.Listener(
-        on_click=on_click,
-        on_scroll=on_scroll,
-        on_move=on_move)
+if record_keyboard:
+	print("release a 2s 'esc' hold to end the recording for keyboard")
 
-keyboard_listener.start()
-mouse_listener.start()
-keyboard_listener.join()
-mouse_listener.join()
+	# Collect events from keyboard until esc is pressed
+	keyboard_listener = keyboard.Listener(
+		on_press=on_press,
+		on_release=on_release)
+
+	keyboard_listener.start()
+	keyboard_listener.join()
+
+if record_mouse:
+	print("release a 2s right click hold to end the recording for mouse")
+
+	# Collect events from mouse until a 2s right click is released
+	mouse_listener = mouse.Listener(
+			on_click=on_click,
+			on_scroll=on_scroll,
+			on_move=on_move)
+
+	mouse_listener.start()
+	mouse_listener.join()
+
+# write back recording
+
+with open('data/{}.txt'.format(name_of_recording), 'w+') as outfile:
+	json.dump(storage, outfile, indent=4)
